e92c71adcd1e0b111b4712e2c0dea29536b23539
dobesv@gmail.com <dobesv@gmail.com@c2e10e96-c8c8-11dd-a412-a1792178251d>
2008-12-13 07:49:31 +0000

    List of clients should be public.
    git-svn-id: http://freshbooks-api-java-client.googlecode.com/svn/trunk@8 c2e10e96-c8c8-11dd-a412-a1792178251d

--- freshbooksApiClient/src/com/freshbooks/model/Clients.java
+++ freshbooksApiClient/src/com/freshbooks/model/Clients.java
@@ -7,7 +7,7 @@ import com.thoughtworks.xstream.annotations.XStreamAlias;
 @XStreamAlias("clients")
 public class Clients extends PagedResponseContent<Client> {
     
-    ArrayList<Client> getClients() {
+    public ArrayList<Client> getClients() {
         return getContents();
     }
 }
